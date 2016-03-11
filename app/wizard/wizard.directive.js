d65b44237c8b239a381be85fe5483f99641a2bdb
mp.apollo <mp.apollo@hotail.com>
2016-03-11 15:40:18 -0300

    Add validation in step 1.

--- app/wizard/wizard.directive.js
+++ app/wizard/wizard.directive.js
@@ -3,47 +3,47 @@
  */
 (function(){
     "use strict";
-	angular
-		.module('dotWidget.wizard', ['angularMoment',
-								     'dotWidget.settings',
-									 'dotWidget.wizard.rideSummary',
-									 'dotWidget.wizard.pickUpForm',
-									 'dotWidget.wizard.vehicleSelection'])
-			.directive('wizard', wizardDirectiveFn);
+    angular
+        .module('dotWidget.wizard', ['angularMoment',
+                                     'dotWidget.settings',
+                                     'dotWidget.wizard.rideSummary',
+                                     'dotWidget.wizard.pickUpForm',
+                                     'dotWidget.wizard.vehicleSelection'])
+            .directive('wizard', wizardDirectiveFn);
 
-	function wizardDirectiveFn(){
-		return {
-			restrict: 'E',
-			replace: true,
-			templateUrl: 'app/wizard/wizard.view.html',
-			controller: wizardControllerFn,
-			require : ['$scope', '$http', 'moment', 'appSettings']
-		};
-	}
+    function wizardDirectiveFn(){
+        return {
+            restrict: 'E',
+            replace: true,
+            templateUrl: 'app/wizard/wizard.view.html',
+            controller: wizardControllerFn,
+            require : ['$scope', '$http', 'moment', 'appSettings']
+        };
+    }
 
-	function wizardControllerFn($scope, $http, moment, appSettings){
-		$scope.stepEneable = 1;
-		$scope.cargando = false;
-		$scope.services = [];
-		$scope.selectedService = {};
-		$scope.appSettings = appSettings;
-		$scope.ride = {
-			type : 'hours',
-			date: moment().add(3, 'days').toDate(),
-			time: new Date(1970, 1, 1, 10, 0),
-			from: null,
-			to: null,
-			hours: null,
-			passengers : null,
-			bags : null,
-			vehicle: {
-				image: null,
-				description: null
-			},
-			total: {
-				currency: null,
-				value: null
-			}
-		};
-	}	
+    function wizardControllerFn($scope, $http, moment, appSettings){
+        $scope.stepEneable = 1;
+        $scope.cargando = false;
+        $scope.services = [];
+        $scope.selectedService = {};
+        $scope.appSettings = appSettings;
+        $scope.ride = {
+            type : 'hours',
+            date: moment().add(3, 'days').toDate(),
+            time: new Date(1970, 1, 1, 10, 0),
+            from: null,
+            to: null,
+            hours: '2',
+            passengers : '1',
+            bags : '0',
+            vehicle: {
+                image: null,
+                description: null
+            },
+            total: {
+                currency: null,
+                value: null
+            }
+        };
+    }	
 })();
